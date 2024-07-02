f8b2808379df51fbfadb740699d6a3be55924d4f
woelffer <alecwoelffer@gmail.com>
2024-07-01 20:40:38 -0500

    Add files via upload
    Updated player movement in an attempt to make it smoother and faster. Still needs work.  Modified Player class "using instance variables instead of class variables provides better design and flexibility"

---
+++ Player.py
@@ -0,0 +1,26 @@
+
+###Player class below
+import pygame
+
+class Player:
+    def __init__(self):
+        self.player_img = pygame.image.load("Biplane.png")
+        self.pos_x, self.pos_y = 500, 500
+        self.vel_x, self.vel_y = 600,600
+
+
+    def printer():
+        print("Printer func")
+
+    def movement(self, movement, dt):
+        if movement == 'a':
+            self.pos_x -= self.vel_x * dt
+            
+        if movement == 'd':
+            self.pos_x += self.vel_x * dt
+          
+        if movement == 'w':
+            self.pos_y -= self.vel_y * dt
+
+        if movement == 's':
+            self.pos_y += self.vel_y * dt
--- pygamedemo.py
+++ pygamedemo.py
@@ -1,43 +1,45 @@
+import Player
 import pygame
+print("hello world")
+
+
+player_model = Player.Player()
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('PygameDemo')
-
-player = pygame.Rect((300, 250, 50, 50))
-groundRect = pygame.Rect(0, 500, 800, 100)
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+running = True
 
-x, y = 300, 400
-xVelocity, yVelocity = 0, 0
-red = (255, 0, 0)
-blue = (0, 0, 255)
 
-run = True
-while run:
-    clock.tick(30)
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, red, player)
-    pygame.draw.rect(screen, blue, groundRect)
-
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-10, 0)
-    elif key[pygame.K_d] == True:
-        player.move_ip(10, 0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0, -10)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, 10)
-
+while running:
+    #Use Delta Time: Implement delta time for frame-independent movement.
+    dt = clock.tick(60) / 1000.0
+    screen.fill((0,0,0))
+    screen.blit(player_model.player_img, (player_model.pos_x, player_model.pos_y))
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
-    pygame.display.update()
+        key = pygame.key.get_pressed()
+        
+        if key[pygame.K_a] == True:
+            player_model.movement('a', dt)
+
+        if key[pygame.K_d] == True:
+            player_model.movement('d', dt)
+
+        if key[pygame.K_w] == True:
+            player_model.movement('w', dt)
+
+        if key[pygame.K_s] == True:
+            player_model.movement('s', dt)
+            
+        if event.type == pygame.QUIT:
+            running = False
+        
+
+    
+    pygame.display.flip()
+
+    clock.tick(120)
 
 pygame.quit()
